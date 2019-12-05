4f35e0582ecf84307d82dcabf204cb82961dd280
teixeira47 <44778476+teixeira47@users.noreply.github.com>
2019-12-05 17:58:28 +0000

    Update Capeias.java

--- src/com/POOProject/Capeias.java
+++ src/com/POOProject/Capeias.java
@@ -1,16 +1,56 @@
 package com.POOProject;
 
 public class Capeias extends Tourada {
-    public Capeias(int assistencia, String data, String nomeEvento) {
-        super(assistencia, data, nomeEvento);
+    private int bilhete = 0;
+    private int total = 0;
+    private String local;
+
+    public Capeias(Tourada t, Local l) {
+        super(t.getAssistencia(),t.getNomeEvento(),t.getDATA());
+        this.total = t.getAssistencia();
+        this.local = l.getLocalidade();
+    }
+
+    public void setBilhete(int bilhete) {
+        this.bilhete = bilhete;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public int getBilhete() {
+        return bilhete;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+   
+    public void comprabilhete(int n){
+        int aux = this.bilhete+n;
+        if (aux <= this.total) {
+            this.bilhete=this.bilhete+n;
+        }else System.out.println("Nao temos bilhetes suficientes");
+       
+    }
+    public void removerbilhete(int n){
+        int aux = this.bilhete-n;
+        if (aux <= this.total && aux>0) {
+            this.bilhete=this.bilhete-n;
+        }else System.out.println("Nao tem mais bilhetes para remover");
+       
     }
 
     @Override
     public String toString() {
-        return "Capeias{" +
-                "assistencia=" + assistencia +
-                ", DATA='" + DATA + '\'' +
-                ", NomeEvento='" + NomeEvento + '\'' +
-                '}';
+        return "Capeia{" + "Tourada:" + super.getNomeEvento() +",  \n Local:"+this.local+ ", \n Total de Bilhetes=" + this.bilhete+ "\n Data:"+super.getDATA()+'}';
     }
+
+
+    
 }
