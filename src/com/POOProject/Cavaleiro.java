785851193c20fa69d737684464c52657d5a5c623
arctumn <silvestre2620092@hotmail.com>
2019-12-05 15:40:22 +0000

    Alteraçoes

--- src/com/POOProject/Cavaleiro.java
+++ src/com/POOProject/Cavaleiro.java
@@ -1,4 +1,43 @@
 package com.POOProject;
 
+import java.util.Objects;
+
 public class Cavaleiro extends Entidade {
+    private Local terra;
+    private static int nVezes;
+
+    public Cavaleiro(String nome, int IDADE, Local terra) {
+        super(nome, IDADE);
+        nVezes++;
+        this.terra = terra;
+    }
+
+    public Local getTerra() {
+        return terra;
+    }
+
+    public void setTerra(Local terra) {
+        this.terra = terra;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cavaleiro)) return false;
+        if (!super.equals(o)) return false;
+        Cavaleiro cavaleiro = (Cavaleiro) o;
+        return Objects.equals(terra, cavaleiro.terra);
+    }
+
+    @Override
+    public String toString() {
+        return "Cavaleiro{" +
+                "terra=" + terra +
+                ", nome='" + nome + '\'' +
+                ", IDADE=" + IDADE +
+                '}';
+    }
+    public void numeroCavaleiros(){
+        System.out.println(nVezes);
+    }
 }
--- src/com/POOProject/Entidade.java
+++ src/com/POOProject/Entidade.java
@@ -1,4 +1,46 @@
 package com.POOProject;
 
+import java.util.Objects;
+
 public class Entidade {
+    protected String nome;
+    protected int IDADE; //idade pode ser o ano de formaçao ou a idade
+
+    public Entidade(String nome, int IDADE) {
+        this.nome = nome;
+        this.IDADE = IDADE;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIDADE() {
+        return IDADE;
+    }
+
+    public void setIDADE(int IDADE) {
+        this.IDADE = IDADE;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Entidade)) return false;
+        Entidade entidade = (Entidade) o;
+        return IDADE == entidade.IDADE &&
+                Objects.equals(nome, entidade.nome);
+    }
+
+    @Override
+    public String toString() {
+        return "Entidade{" +
+                "nome='" + nome + '\'' +
+                ", IDADE=" + IDADE +
+                '}';
+    }
 }
