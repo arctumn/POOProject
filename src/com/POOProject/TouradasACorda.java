ea691b56bb0b94caeb652017c5f0d5618e70ce01
farsenfsd <farsenfsd@gmail.com>
2019-12-06 18:22:13 +0000

    Também acabada, digam-me se precisarem de algo    - Luís

--- src/com/POOProject/TouradasACorda.java
+++ src/com/POOProject/TouradasACorda.java
@@ -1,16 +1,28 @@
 package com.POOProject;
 
+import java.util.Comparator;
+
 public class TouradasACorda extends Tourada {
-    public TouradasACorda(int assistencia, String data, String nomeEvento) {
-        super(assistencia, data, nomeEvento);
+    private String local;
+
+    public TouradasACorda(Tourada t, Local l) {
+        super(t.getAssistencia(),t.getNomeEvento(),t.getDATA());
+        this.local = l.getLocalidade();
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getLocal() {
+        return local;
     }
 
     @Override
     public String toString() {
-        return "TouradasACorda{" +
-                "assistencia=" + assistencia +
-                ", DATA='" + DATA + '\'' +
-                ", NomeEvento='" + NomeEvento + '\'' +
-                '}';
+        return "Tourada À Corda{" + "Tourada:" + super.getNomeEvento() +",  \n Local:"+this.local + "\n Data:"+super.getDATA()+'}';
     }
+
+    public static Comparator<TouradasACorda> compareTouradasACordabyNome = (o1, o2) -> o1.getNomeEvento().compareTo(o2.getNomeEvento());
+
 }
