68b063060ca7667d6dccb3011d7776860bc17edd
farsenfsd <farsenfsd@gmail.com>
2019-12-06 18:20:36 +0000

    Em princípio está acabada. Largadas não têm bilhetes nem assistência definida (são feitas na rua) pelo que não faz sentido adicionar esses campos.    - Luís

--- src/com/POOProject/Largadas.java
+++ src/com/POOProject/Largadas.java
@@ -1,4 +1,28 @@
 package com.POOProject;
 
+import java.util.Comparator;
+
 public class Largadas extends Tourada {
+    private String local;
+
+    public Largadas(Tourada t, Local l) {
+        super(t.getAssistencia(),t.getNomeEvento(),t.getDATA());
+        this.local = l.getLocalidade();
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    @Override
+    public String toString() {
+        return "Largada{" + "Tourada:" + super.getNomeEvento() +",  \n Local:"+this.local + "\n Data:"+super.getDATA()+'}';
+    }
+
+    public static Comparator<Largadas> compareLargadasbyNome = (o1, o2) -> o1.getNomeEvento().compareTo(o2.getNomeEvento());
+
 }
