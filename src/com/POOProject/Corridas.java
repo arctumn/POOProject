8d1f435af7be251aa5c84dbdecb563b1997bdba5
teixeira47 <44778476+teixeira47@users.noreply.github.com>
2019-12-06 14:22:22 +0000

    Update Corridas.java

--- src/com/POOProject/Corridas.java
+++ src/com/POOProject/Corridas.java
@@ -1,7 +1,58 @@
 package com.POOProject;
 
 public class Corridas extends Tourada {
-    public Corridas(int assistencia, String data, String nomeEvento) {
-        super(assistencia, data, nomeEvento);
+    private int bilhete = 0;
+    private int total = 0;
+    private String local;
+    
+    public Corridas(Tourada t, Local l) {
+        super(t.getAssistencia(),t.getNomeEvento(),t.getDATA());
+        this.total = t.getAssistencia();
+        this.local = l.getLocalidade();
     }
+    public void setBilhete(int bilhete) {
+        this.bilhete = bilhete;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public int getBilhete() {
+        return bilhete;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+   
+    public void comprabilhete(int n){
+        int aux = this.bilhete+n;
+        if (aux <= this.total) {
+            this.bilhete=this.bilhete+n;
+        }else System.out.println("Nao temos bilhetes suficientes");
+       
+    }
+    public void removerbilhete(int n){
+        int aux = this.bilhete-n;
+        if (aux <= this.total && aux>0) {
+            this.bilhete=this.bilhete-n;
+        }else System.out.println("Nao tem mais bilhetes para remover");
+       
+    }
+    public void percentagem(){
+        int aux = (this.bilhete/this.total)*100;
+        System.out.println("A assistência foi de "+aux+"%");
+    }
+
+
+    @Override
+    public String toString() {
+        return "Corridas{" + "Tourada:" + super.getNomeEvento() +",  \n Local:"+this.local+ ", \n Total de Bilhetes=" + this.bilhete+ "\n Data:"+super.getDATA()+'}';
+    }
+    
 }
